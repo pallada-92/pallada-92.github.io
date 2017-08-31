17bef7968baa8674988abcef004c5ca8a1bf0e2d
Yaroslav Sergienko <pallada-92@ya.ru>
2017-08-31 17:11:46 +0300

    v2

--- 170315_sphere/sphere-jump.js
+++ 170315_sphere/sphere-jump.js
@@ -33,7 +33,7 @@ function SphereJump(params) {
       items: params.items,
     }
   ); */
-  this.images = {'bg_pc.png': null, 'sphere_static.png': null};
+  this.images = {'bg_pc_small.png': null, 'sphere_static.png': null};
   for (var i in params.items) {
     this.images[params.items[i].icon] = null;
   }
@@ -56,10 +56,10 @@ function SphereJump(params) {
   this.half_easing = function(x) {
     return - Math.cos((x + 1) * Math.PI / 2);
   }
-  this.draw = function(t) {
+  this.draw = function(t, draw_note) {
     var ctx = this.ctx;
     var img;
-    img = this.images['bg_pc.png'];
+    img = this.images['bg_pc_small.png'];
     var note_h = this.h * 1.2;
     var note_w = img.width * note_h / img.height;
     var note_x0 = this.w - note_w * 1.05;
@@ -67,7 +67,10 @@ function SphereJump(params) {
     var note_cx = note_x0 + note_w * 0.6;
     var note_cy = note_y0 + note_h * 0.33;
     var note_rad = note_h * 0.27;
-    ctx.drawImage(img, note_x0, note_y0, note_w, note_h);
+    if (draw_note) {
+      ctx.drawImage(img, note_x0, note_y0, note_w, note_h);
+      return;
+    }
     img = this.images['sphere_static.png'];
     var sphere_w = note_rad * 2;
     var sphere_h = note_rad * 2 / img.width * img.height;
@@ -75,8 +78,8 @@ function SphereJump(params) {
     var pt1 = [this.w / 2, this.h * 0.7];
     var pt2 = [note_cx, note_cy];
     var pt = [
-      (this.w - note_cx) + t * (2 * note_cx - this.w),
-      note_cy + t * (1 - t) * 4 * note_cy,
+      this.w / 2 + t * (note_cx - this.w / 2),
+      note_cy + t * (1 - t) * 4 * note_cy - 100 * (1 - t),
     ]
     var sphere_x, sphere_y;
     ctx.drawImage(
@@ -93,7 +96,6 @@ function SphereJump(params) {
     */
   }
   this.t = 0;
-
   var requestAnimationFrame = window.requestAnimationFrame;
   if (!requestAnimationFrame) {
     requestAnimationFrame = (function() {
@@ -117,19 +119,27 @@ function SphereJump(params) {
       var ctx = this.ctx;
       ctx.clearRect(0, 0, this.w, this.h);
       ctx.save();
-      ctx.globalAlpha = 0.6;
-      var t1 = this.t;
-      var dt0 = 1 / 16;
-      this.draw(this.half_easing(Math.min(1, this.t)));
-      this.t += dt0;
-      this.draw(this.half_easing(Math.min(1, this.t)));
-      this.t += dt0;
-      this.draw(this.half_easing(Math.min(1, this.t)));
-      this.t = t1 + 1 / 25;
-      ctx.restore();
+      var t;
+      var dt = 1 / 5;
+      t = Math.floor(this.t / dt) * dt;
+      this.draw(0, 1);
+      if (this.t >= 1) {
+        this.draw(1);
+      } else {
+        ctx.globalAlpha = (1 - (this.t - t) / dt) * this.t;
+        var s = ctx.globalAlpha;
+        this.draw(this.half_easing(Math.min(1, t)));
+        t += dt;
+        ctx.globalAlpha = (1 - (t - this.t) / dt) * this.t;
+        s += ctx.globalAlpha;
+        // console.log(s);
+        this.draw(this.half_easing(Math.min(1, t)));
+        ctx.restore();
+        this.t += params.speed;
+      }
       setTimeout(function() {
         requestAnimationFrame(f);
-      }, 1000 / 24);
+      }, 1000 * params.speed);
     }.bind(this);
     f();
   }
