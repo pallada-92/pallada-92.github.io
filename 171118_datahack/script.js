7b845290fe91d330849567ddf36b48114ba8efdd
Yaroslav Sergienko <pallada-92@ya.ru>
2017-11-19 11:41:43 +0300

    datahack

--- 171118_datahack/script.js
+++ 171118_datahack/script.js
@@ -15,80 +15,347 @@ var camera, controls, scene, renderer,
     loader = new THREE.TextureLoader(),
     map_texture, map_ratio = 8500 / 11000;
 
-loader.load(
-	'map_inv_70.jpg',
+window.addEventListener('load', load_map);
+
+function load_map() {
+  loader.load(
+	'map_inv_70_edited.jpg',
 	function ( texture ) {
       map_texture = texture;
       init();
       render();
 	},
 	function ( xhr ) {
-		console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+	  console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 	},
 	function ( xhr ) {
-		console.log('Texture loading error');
+	  console.log('Texture loading error');
 	}
-);
+  );
+}
+
+var cx = 550, cy = -1000, cz = 0.05,
+    clat = 37.497395, clon = 55.867085,
+    dx = 20623.56725, dy = -55867.085,
+    coord_matrix = new THREE.Matrix4(),
+    corners_geo = [
+      [37.47955, 37.51524],
+      [55.88045, 55.85372],
+    ],
+    map_geo = [
+      [37.47955 * cx - dx, 37.51524 * cx - dx],
+      [55.88045 * cy - dy, 55.85372 * cy - dy],
+    ],
+    map_geo_center = [
+      (map_geo[0][0] + map_geo[0][1]) / 2,
+      (map_geo[1][0] + map_geo[1][1]) / 2,
+    ],
+    map_px = [
+      [356 / 5950, 5314 / 5950],
+      [1 - 6887 / 7700, 1 - 282 / 7700],
+    ],
+    map_geo_pts = [
+      [map_geo[0][0], map_geo[1][0]],
+      [map_geo[0][0], map_geo[1][1]],
+      [map_geo[0][1], map_geo[1][1]],
+      [map_geo[0][1], map_geo[1][0]],
+    ],
+    map_px_pts = [
+      new THREE.Vector2(map_px[0][0], map_px[1][0]),
+      new THREE.Vector2(map_px[0][0], map_px[1][1]),
+      new THREE.Vector2(map_px[0][1], map_px[1][1]),
+      new THREE.Vector2(map_px[0][1], map_px[1][0]),
+    ],
+    map_geometry = new THREE.PlaneGeometry(
+      Math.abs(map_geo[0][0] - map_geo[0][1]),
+      Math.abs(map_geo[1][0] - map_geo[1][1]),
+    );
+coord_matrix.makeScale(cx, 1, cy);
+
+map_geometry.rotateX(-Math.PI / 2 );
+map_geometry.translate(map_geo_center[0], 0, map_geo_center[1]);
+map_geometry.faceVertexUvs = [[
+  [map_px_pts[1], map_px_pts[0], map_px_pts[2]],
+  [map_px_pts[0], map_px_pts[3], map_px_pts[2]],
+]];
+var col = 0x999999,
+    grid_helper = new THREE.GridHelper(20, 2000, col, col);
+grid_helper.applyMatrix(coord_matrix);
+grid_helper.position.set(37.600 * cx - dx, -0.1, 55.750 * cy - dy);
+
+var light = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+light.position.set( 0, 50, 0 );
 
 function flag(params) {
   var res = new THREE.Group();
   return res;
 }
 
+var needle_mat = new THREE.LineBasicMaterial({
+	color: 0xffffff,
+}),
+    yellow_mat = new THREE.MeshNormalMaterial({
+      side: THREE.DoubleSide,
+    }),
+    /*yellow_mat = new THREE.MeshLambertMaterial({
+      color: 'yellow',
+      
+    });*/
+    heatmap_mat = new THREE.MeshBasicMaterial({
+      // color: 'yellow',
+      transparent: true,
+      blending: THREE.AdditiveBlending,
+      vertexColors: THREE.VertexColors,
+      side: THREE.DoubleSide,
+    }),
+    map_mat;
+
+
+function needle(params) {
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(
+    new THREE.Vector3(
+      params.lat * cx - dx, 0, params.lon * cy - dy),
+	new THREE.Vector3(
+      params.lat * cx - dx, params.height * cz, params.lon * cy - dy),
+  );
+  return new THREE.Line(geometry, needle_mat);
+}
+
+function flag(params) {
+  var res = new THREE.Group(),
+      x = params.lat * cx - dx, y = params.lon * cy - dy,
+      c = 0.05;
+  for (var i=0; i<params.segments.length; i++) {
+    var seg = params.segments[i],
+        shape = new THREE.Shape();
+    shape.moveTo(cz * seg.minh, seg.maxwmin * c);
+    shape.lineTo(cz * seg.minh, c * (seg.minwmin || 0));
+    shape.lineTo(cz * seg.maxh, c * (seg.minwmax || 0));
+    shape.lineTo(cz * seg.maxh, c * seg.maxwmax);
+    shape.closePath();
+    var geometry = new THREE.ShapeGeometry(shape);
+    geometry.rotateZ(Math.PI / 2);
+    geometry.rotateY(params.dir);
+    geometry.translate(x, 0, y);
+    var mesh = new THREE.Mesh(geometry, seg.mat);
+    res.add(mesh);
+  }
+  return res;
+}
+
+function add_cafes_flags(scene) {
+  var needles = {},
+  results = cafes.result;
+  for (var i=0; i<results.length; i++) {
+    var item = results[i],
+        coords = item.geoData.coordinates,
+        hash = coords[0].toFixed(12) + ':' + coords[1].toFixed(12);
+    if (!(hash in needles)) {
+      needles[hash] = [];
+    }
+    item.hash = hash;
+    needles[hash].push(item);
+  }
+  for (var hash in needles) {
+    var t = hash.split(':'),
+        lat = +t[0], lon = +t[1],
+        items = needles[hash],
+        angle = 2 * Math.PI / items.length;
+    scene.add(needle({
+      lon: lon, lat: lat,
+      height: 20,
+    }));
+    for (var i=0; i<items.length; i++) {
+      var item = items[i],
+          year1 = +item.LicenseBegin.split('.')[2],
+          year2 = +item.LicenseExpire.split('.')[2];
+      scene.add(flag({
+        lat: lat, lon: lon, dir: angle * i,
+        segments: [
+          {
+            minh: Math.max(0, year1 - 2000),
+            maxh: Math.min(20, year2 - 2000),
+            maxwmin: 1, maxwmax: 1,
+            minwmin: 0, minwmax: 0,
+            mat: yellow_mat
+          },
+        ],
+      }));
+    }
+  }
+}
+
+function add_flats_flags(scene) {
+  var needles = {},
+      results = window.flats.result;
+  for (var i=0; i<results.length; i++) {
+    var item = results[i],
+        coords = [item.Lon, item.Lat],
+        hash = coords[0].toFixed(12) + ':' + coords[1].toFixed(12);
+    if (hash in needles) {
+      throw 'multiple flats';
+    }
+    item.hash = hash;
+    needles[hash] = item;
+  }
+  for (var hash in needles) {
+    var t = hash.split(':'),
+        lat = +t[0], lon = +t[1],
+        item = needles[hash];
+        // angle = 2 * Math.PI / items.length;
+    scene.add(needle({
+      lon: lon, lat: lat,
+      height: item.Flats / 10,
+    }));
+  }
+}
+
+function make_grid_fun(min, max, count) {
+  var inverse = function(x) {
+    var idx = (x - min) / (max - min) * (count - 1);
+    return Math.max(0, Math.min(count - 1, Math.round(idx)));
+  };
+  var res = function(rmin, rmax) {
+    var idx_min = inverse(rmin),
+        idx_max = inverse(rmax),
+        pts = [];
+    // console.log(min, max, count, rmin, rmax);
+    // console.log(idx_min, idx_max);
+    for (var i=idx_min; i<=idx_max; i++) {
+      var t = (i - idx_min) / (idx_max - idx_min),
+          x = t * (max - min) + min;
+      pts.push(i, x);
+    }
+    return pts;
+  };
+  res.count = count;
+  res.min = min;
+  res.max = max;
+  return res;
+}
+
+function make_heatmap(pts, rad, coeff, gridx, gridy) {
+  var res = new Uint32Array(gridx.count * gridy.count),
+      zero_rad = rad * 2;
+  res.w = gridx.count;
+  res.h = gridy.count;
+  for (var i=0; i<pts.length; i++) {
+    var pt = pts[i],
+        pts_x = gridx(pt[0] - zero_rad, pt[0] + zero_rad),
+        pts_y = gridy(pt[1] - zero_rad, pt[1] + zero_rad);
+    // console.log(pts_x, pts_y);
+    for (var ix = 0; ix<pts_x.length; ix+=2) {
+      var iix = pts_x[ix],
+          x = pts_x[ix + 1],
+          dx = pt[0] - x;
+      for (var iy = 0; iy<pts_y.length; iy+=2) {
+        var iiy = pts_y[iy],
+            y = pts_y[iy + 1],
+            dy = pt[1] - y,
+            dist = dx * dx + dy * dy,
+            val = coeff * pt[2] * Math.exp(- dist / (rad * rad));
+        // console.log(val);
+        res[iiy * gridx.count + iix] += Math.round(val);
+      }
+    }
+  }
+  return res;
+}
+
+function add_heatmap_flags(scene, heatmap, gridx, gridy) {
+  var pts_x = gridx(gridx.min, gridx.max),
+      pts_y = gridy(gridy.min, gridy.max);
+  for (var ix=0; ix<pts_x.length; ix+=2) {
+    var iix = pts_x[ix],
+        x = pts_x[ix + 1];
+    for (var iy=0; iy<pts_y.length; iy+=2) {
+      var iiy = pts_y[iy],
+          y = pts_y[iy + 1];
+      scene.add(needle({
+        lat: x, lon: y,
+        height: heatmap[iiy * heatmap.w + iix],
+      }));
+    }
+  }
+}
+
+function heatmap_mesh(
+  heatmap, gridx, gridy, map_px, max_height, step, color
+) {
+  var geometry = new THREE.BufferGeometry(),
+      positions = new Float32Array(gridx.count * gridy.count * 3),
+      colors = new Float32Array(gridx.count * gridy.count * 3),
+      uvs = new Float32Array(gridx.count * gridy.count * 2),
+      index = new Array((gridx.count - 1) * (gridy.count - 1) * 6),
+      pts_x = gridx(gridx.min, gridx.max),
+      pts_y = gridy(gridy.min, gridy.max);
+  for (var ix=0; ix<pts_x.length; ix+=2) {
+    var iix = pts_x[ix],
+        x = pts_x[ix + 1],
+        tx = ix / (pts_x.length - 1);
+    for (var iy=0; iy<pts_y.length; iy+=2) {
+      var iiy = pts_y[iy],
+          y = pts_y[iy + 1],
+          base = iiy * heatmap.w + iix,
+          ty = iy / (pts_y.length - 1);
+      positions[base * 3] = x * cx - dx;
+      positions[base * 3 + 1] = heatmap[base] * cz;
+      positions[base * 3 + 2] = y * cy - dy;
+      colors[base * 3] = heatmap[base] * cz / 5;
+      colors[base * 3 + 1] = 0;
+      colors[base * 3 + 2] = 0;
+      uvs[base * 2] =
+        tx * (map_px[0][1] - map_px[0][0]) + map_px[0][0];
+      uvs[base * 2 + 1] =
+        ty * (map_px[1][1] - map_px[1][0]) + map_px[1][0];
+    }
+  }
+  var i=0;
+  for (var ix=0; ix<heatmap.w - 1; ix++) {
+    for (var iy=0; iy<heatmap.h - 1; iy++) {
+      var v1 = iy * heatmap.w + ix,
+          v2 = (iy + 1) * heatmap.w + ix;
+      index[i++] = v1;
+      index[i++] = v1 + 1;
+      index[i++] = v2;
+      //
+      index[i++] = v2 + 1;
+      index[i++] = v2;
+      index[i++] = v1 + 1;
+    }
+  }
+  geometry.setIndex(index);
+  geometry.addAttribute(
+    'position', new THREE.BufferAttribute(positions, 3));
+  // geometry.addAttribute(
+    // 'color', new THREE.BufferAttribute(colors, 3));
+  geometry.addAttribute(
+    'uv', new THREE.BufferAttribute(uvs, 2));
+  return new THREE.Mesh(geometry, map_mat);
+}
+
 function init() {
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x333333);
-  renderer = new THREE.WebGLRenderer();
+  scene.background = new THREE.Color(0x000000);
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   var container = document.getElementById('root');
   container.appendChild(renderer.domElement);
   camera = new THREE.PerspectiveCamera(
     60, window.innerWidth / window.innerHeight,
-    0.1, 3600
+    0.1, 100
   );
   window.addEventListener('resize', onWindowResize, false);
   map_texture.minFilter = THREE.LinearFilter;
-  var cx = 0.55, cy = -1,
-      coord_matrix = new THREE.Matrix4(),
-  map_geo = [
-    [37479.55 * cx, 37515.24 * cx],
-    [55880.45 * cy, 55853.72 * cy]
-  ],
-      map_geo_center = [
-        (map_geo[0][0] + map_geo[0][1]) / 2,
-        (map_geo[1][0] + map_geo[1][1]) / 2,
-      ],
-      map_px = [
-                [356 / 5950, 5314 / 5950],
-                [1 - 6887 / 7700, 1 - 282 / 7700],
-               ],
-      map_geo_pts = [
-        [map_geo[0][0], map_geo[1][0]],
-        [map_geo[0][0], map_geo[1][1]],
-        [map_geo[0][1], map_geo[1][1]],
-        [map_geo[0][1], map_geo[1][0]],
-      ],
-      map_px_pts = [
-        new THREE.Vector2(map_px[0][0], map_px[1][0]),
-        new THREE.Vector2(map_px[0][0], map_px[1][1]),
-        new THREE.Vector2(map_px[0][1], map_px[1][1]),
-        new THREE.Vector2(map_px[0][1], map_px[1][0]),
-      ],
-    map_material = new THREE.MeshBasicMaterial({
-	map: map_texture,
-  }),
-      map_geometry = new THREE.PlaneGeometry(
-        Math.abs(map_geo[0][0] - map_geo[0][1]),
-        Math.abs(map_geo[1][0] - map_geo[1][1]),
-      );
-  coord_matrix.makeScale(cx, 1, cy);
   camera.position.set(
     map_geo_center[0],
     10,
     map_geo_center[1] + 10,
   );
-  console.log(map_geo_center);
   controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.addEventListener('change', render);
   controls.enableZoom = true;
@@ -96,23 +363,28 @@ function init() {
     map_geo_center[0], 0, map_geo_center[1]
   )
   camera.lookAt(controls.target);
-  // map_geometry.rotateX(Math.PI);
-  map_geometry.rotateX(-Math.PI / 2 );
-  map_geometry.translate(map_geo_center[0], 0, map_geo_center[1]);
-  map_geometry.faceVertexUvs = [[
-    [map_px_pts[1], map_px_pts[0], map_px_pts[2]],
-    [map_px_pts[0], map_px_pts[3], map_px_pts[2]],
-  ]];
-  var map_plane = new THREE.Mesh(map_geometry, map_material);
-  scene.add(map_plane);
-  var col = 0xffffff,
-      grid_scale =  50,
-      grid_helper = new THREE.GridHelper(2000, 200, col, col);
-  grid_helper.applyMatrix(coord_matrix);
-  grid_helper.position.set(37600 * cx, 0.1, 55750 * cy);
-  console.log(grid_helper);
+  map_mat = new THREE.MeshBasicMaterial({
+	map: map_texture,
+  });
+  var map_plane = new THREE.Mesh(map_geometry, map_mat);
+  // scene.add(map_plane);
   scene.add(grid_helper);
-  // scene.applyMatrix(matrix);
+  // scene.add(light);
+  // add_cafes_flags(scene);
+  // add_flats_flags(scene);
+  var c = cx / 1000,
+      gridx1 = make_grid_fun(clat - 0.017845, clat + 0.017845, 50),
+      gridx2 = make_grid_fun(
+        gridx1.min * c, gridx1.max * c, gridx1.count),
+      gridy = make_grid_fun(clon - 0.013365, clon + 0.013365, 50),
+      hmap = make_heatmap(
+        [[clat * c, clon, 2]],
+        0.005, 100,
+        gridx2, gridy,
+      );
+  var mesh = heatmap_mesh(hmap, gridx1, gridy, map_px);
+  console.log(mesh);
+  scene.add(mesh);
 }
 
 function onWindowResize() {
